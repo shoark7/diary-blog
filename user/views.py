ddd452fb2c50ee621f71416429444b7739b41f34
Sunghwan Park <shoark7@gmail.com>
2017-07-19 11:24:58 +0900

    Make sign up and login view

--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from . import views
+from .models import BlogUser
 
 
 # 로그인, 로그아웃, 회원가입
@@ -12,10 +13,11 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         context = {'error_messages': ''}
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(username=username, password=password)
         if user is not None:
             auth_login(request, user)
-            return render(request, 'blog/index.html' )
+            return render(request, 'blog/index.html')
+            # return redirect('blog:index')
         else:
             context['error_messages'] = '다시 로그인하세요. 잘못된 입력입니다.'
             return render(request, 'user/login.html', context)
@@ -29,4 +31,30 @@ def logout(request):
 
 
 def sign_up(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        name = request.POST.get('name')
+        phonenumber = request.POST.get('phonenumber')
+        sex = request.POST.get('sex')
+
+        if password1 != password2:
+            context = {'error_messages': '비밀번호가 다릅니다.'}
+            return render(request, 'user/sign_up.html', context)
+        try:
+            user = BlogUser.objects.create_user(username=username,
+                            password=password1,
+                            name=name,
+                            phonenumber=phonenumber,
+                            sex=sex)
+        except:
+            context = {'error_messages': '뭔가 문제가..'}
+            return render(request, 'user/sign_up.html', context)
+        else:
+            user = authenticate(username=username, password=password1)
+            auth_login(request, user)
+            # return redirect('blog:index')
+            return render(request, 'blog/index.html')
+    else:
+        return render(request, 'user/sign_up.html')
